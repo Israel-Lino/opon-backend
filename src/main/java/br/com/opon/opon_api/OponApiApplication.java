ac9347feea86e35bb811fb363f6a0d07df3d35bd
Gabriel Leal <gabriel-o-carinha@hotmail.com>
2025-05-22 12:53:37 -0300

    feat: adiciona projeto

---
+++ src/main/java/br/com/opon/opon_api/OponApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.opon.opon_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OponApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OponApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/opon/opon_api/OponApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.opon.opon_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OponApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

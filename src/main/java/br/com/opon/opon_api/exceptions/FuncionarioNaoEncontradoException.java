052ab9998615b5794ac4dad94166399023e24c11
Gabriel Leal <gabriel-o-carinha@hotmail.com>
2025-06-07 19:56:29 -0300

    feat: adiciona Exceptions personalizadas

---
+++ src/main/java/br/com/opon/opon_api/exceptions/FuncionarioNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.com.opon.opon_api.exceptions;
+
+public class FuncionarioNaoEncontradoException extends Exception {
+    public FuncionarioNaoEncontradoException(String message) {
+        super(message);
+    }
+}
